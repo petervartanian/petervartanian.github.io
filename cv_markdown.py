8ba8861ae25948c403c5302e14cb21701c76dc82
Peter Vartanian <peter.vartanian@icloud.com>
2026-09-11 13:13:09 -0400

    Make school distinctions collapsible and closed by default

--- cv_markdown.py
+++ cv_markdown.py
@@ -23,6 +23,7 @@ def export_cv(html):
         lines = [headers, ['---', '---']] + rows
         return '\n\n' + '\n'.join('| ' + ' | '.join(row) + ' |' for row in lines) + '\n\n'
 
+    html = re.sub(r'<summary>(.*?)</summary>', r'<h4>\1</h4>', html)
     html = re.sub(r'<a[^>]*class="download-button"[^>]*>.*?</a>', '', html)
     html = html.replace('Shaded entries', 'Bold entries')
     html = re.sub(r'<p class="experience-note"[^>]*><span class="note-badge">(.*?)</span><span>(.*?)</span></p>',
